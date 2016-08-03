b1be70a027fc1d85c11efcb5341da68a9e822d02
Tiffany Bennett <tiffany@stormbit.net>
2016-08-03 13:18:20 -0400

    Add rustyline support

--- src/bin/rink.rs
+++ src/bin/rink.rs
@@ -1,7 +1,36 @@
 extern crate rink;
+#[cfg(feature = "rustyline")]
+extern crate rustyline;
 
 use rink::*;
 
+#[cfg(feature = "rustyline")]
+fn main() {
+    use rustyline::error::ReadlineError;
+    use rustyline::Editor;
+
+    let mut rl = Editor::new();
+    let mut ctx = load();
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                match one_line(&mut ctx, &*line) {
+                    Ok(v) => ctx.print(&v),
+                    Err(e) => println!("{}", e)
+                };
+            },
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Readline: {:?}", err);
+                break
+            },
+        }
+    }
+}
+
+#[cfg(not(feature = "rustyline"))]
 fn main() {
     use std::io::{stdin, stdout, Write};
 
