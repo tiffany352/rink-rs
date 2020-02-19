f1e241589104245831fe4d77ff6632d73384e7cc
Mikkel Kroman <mk@uplink.io>
2020-02-19 20:35:57 +0100

    Update irc to v0.13.6

--- irc/src/main.rs
+++ irc/src/main.rs
@@ -2,14 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-extern crate irc;
 extern crate glob;
+extern crate irc;
 extern crate rink;
 
 fn main() {
+    use glob::glob;
     use irc::client::prelude::*;
     use rink::*;
-    use glob::glob;
     use std::thread;
 
     #[cfg(feature = "sandbox")]
@@ -23,48 +23,52 @@ fn main() {
         ctx.short_output = true;
         match one_line(&mut ctx, line) {
             Ok(v) => v,
-            Err(e) => e
+            Err(e) => e,
         }
     }
 
     fn run(config: &str) {
-        let server = IrcServer::new(config).unwrap();
+        let server = IrcClient::new(config).unwrap();
         server.identify().unwrap();
         let nick = server.config().nickname.clone().unwrap();
         let prefix = ".c ";
-        server.for_each_incoming(|message| {
-            match message.command {
-                Command::PRIVMSG(ref chan, ref message_str) => {
-                    let prefixed = message_str.starts_with(&*prefix);
+        server
+            .for_each_incoming(|message| {
+                match message.command {
+                    Command::PRIVMSG(ref chan, ref message_str) => {
+                        let prefixed = message_str.starts_with(&*prefix);
 
-                    if prefixed || &*chan == &*nick {
-                        let reply_to = if &*chan == &*nick {
-                            message.source_nickname().unwrap()
-                        } else {
-                            &*chan
-                        };
-                        let line = if prefixed {
-                            message_str[prefix.len()..].trim()
-                        } else {
-                            &message_str[..]
-                        };
-                        let mut i = 0;
-                        let reply = eval(line);
-                        for reply_line in reply.lines() {
-                            if reply_line.trim().len() > 0 {
-                                server.send_privmsg(reply_to, &format!("\x0310> {}", reply_line)).unwrap();
-                                i += 1;
-                            }
-                            // cut off early
-                            if i > 4 {
-                                break;
+                        if prefixed || &*chan == &*nick {
+                            let reply_to = if &*chan == &*nick {
+                                message.source_nickname().unwrap()
+                            } else {
+                                &*chan
+                            };
+                            let line = if prefixed {
+                                message_str[prefix.len()..].trim()
+                            } else {
+                                &message_str[..]
+                            };
+                            let mut i = 0;
+                            let reply = eval(line);
+                            for reply_line in reply.lines() {
+                                if reply_line.trim().len() > 0 {
+                                    server
+                                        .send_privmsg(reply_to, &format!("\x0310> {}", reply_line))
+                                        .unwrap();
+                                    i += 1;
+                                }
+                                // cut off early
+                                if i > 4 {
+                                    break;
+                                }
                             }
                         }
                     }
+                    _ => (),
                 }
-                _ => (),
-            }
-        }).unwrap();
+            })
+            .unwrap();
         println!("Thread {} exiting", config);
     }
 
@@ -72,7 +76,7 @@ fn main() {
     for config in glob("servers/*.json").expect("Glob failed") {
         match config {
             Ok(config) => threads.push(thread::spawn(move || run(config.to_str().unwrap()))),
-            Err(e) => println!("{:?}", e)
+            Err(e) => println!("{:?}", e),
         }
     }
     for thread in threads {
