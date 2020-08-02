b9d3926448cb004eef1c22335e1ba261757fc655
Tiffany Bennett <tiffnixen@gmail.com>
2020-08-02 08:39:09 -0700

    Switch to svelte

---
+++ web/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="typescript">
+  import { Query } from "~/../rink-js/Cargo.toml";
+
+  export let queryText: string = "";
+  export let result: Query | null = null;
+
+  export function handleChange(
+    event: Event & { target: EventTarget & HTMLInputElement }
+  ) {
+    console.log("handleChange");
+    result = new Query(queryText);
+  }
+</script>
+
+<svelte:head>
+  <title>Rink</title>
+</svelte:head>
+
+<p>hello from svelte</p>
+<input bind:value={queryText} on:change={handleChange} />
+<pre>{JSON.stringify(result?.getExpr(), null, 2)}</pre>
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,11 @@
 import "@webcomponents/webcomponentsjs/webcomponents-loader.js";
 import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
-import { Query } from "~/../rink-js/Cargo.toml";
-import "./App";
+import App from "./App.svelte";
 
 console.log("hello world");
+const app = new App({
+  target: document.body,
+  props: {},
+});
+
+export default app;
