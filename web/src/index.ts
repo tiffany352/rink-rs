0d8aeb1e76ca749ea57a41475f5f28f6b3e1b116
Tiffany Bennett <tiffnixen@gmail.com>
2020-08-02 08:45:19 -0700

    Remove web components polyfill

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,3 @@
-import "@webcomponents/webcomponentsjs/webcomponents-loader.js";
-import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
 import App from "./App.svelte";
 
 console.log("hello world");
