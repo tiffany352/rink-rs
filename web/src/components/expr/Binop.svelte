d3c150f1dc4b728759090979193b94e327c5b730
Tiffany Bennett <tiffnixen@gmail.com>
2020-08-06 13:48:17 -0700

    Fix Binop component

--- web/src/components/expr/Binop.svelte
+++ web/src/components/expr/Binop.svelte
@@ -2,17 +2,18 @@
   import * as expr from "../../util/expr";
   import * as prec from "../../util/precedence";
   import PrecedenceWrapper from "../PrecedenceWrapper.svelte";
+  import Node from "./Node.svelte";
 
   export let value: expr.BinOpExpr;
   export let precedence: prec.Precedence;
 </script>
 
-<PrecedenceWrapper {precedence} expected={prec.expectedPrecedence(value.op)}>
-  <svelte:self
-    value={value.left}
-    precedence={prec.expectedPrecedence(value.op) - 1} />
-  <span>{prec.symbol(value.op)}</span>
-  <svelte:self
+<!-- prettier-ignore -->
+<PrecedenceWrapper {precedence} expected={prec.expectedPrecedence(value.op)}
+  ><Node value={value.left} precedence={prec.expectedPrecedence(value.op) - 1}
+  /><span
+    >{prec.symbol(value.op)}</span
+  ><Node
     value={value.right}
-    precedence={prec.expectedPrecedence(value.op) - 1} />
-</PrecedenceWrapper>
+    precedence={prec.expectedPrecedence(value.op) - 1}
+/></PrecedenceWrapper>
