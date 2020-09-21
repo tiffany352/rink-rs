f7dec7f4097c5889186929eab87d377363718e11
Tiffany Bennett <tiffnixen@gmail.com>
2020-09-20 19:27:24 -0700

    Error handling in rink-web

--- web/src/routes/data/_cached.ts
+++ web/src/routes/data/_cached.ts
@@ -2,9 +2,10 @@ import { Request, Response as ExpressResponse } from "express";
 import createEtag from "etag";
 
 interface CachedValue {
-  body: string;
+  status: number;
   time: Date;
   etag: string;
+  body: string;
 }
 
 export default function cached(
@@ -14,14 +15,31 @@ export default function cached(
   let cache: CachedValue | null = null;
   return async (req: Request, res: ExpressResponse) => {
     if (!cache || cache.time.getTime() - new Date().getTime() > cacheTimeMs) {
-      const value = await target();
-      const body = JSON.stringify(value);
-      const time = new Date();
-      const etag = createEtag(body);
-      cache = { body, etag, time };
+      try {
+        const value = await target();
+        const body = JSON.stringify(value);
+        const time = new Date();
+        const etag = createEtag(body);
+        cache = { status: 200, body, etag, time };
+      } catch (err) {
+        console.log("Error updating cached resource:", err);
+        const time = new Date();
+        const body = JSON.stringify({
+          code: err.code,
+          message: err.toString(),
+          date: time.toISOString(),
+        });
+        const etag = createEtag(body);
+        cache = {
+          status: 503,
+          body,
+          etag,
+          time,
+        };
+      }
     }
 
-    res.set({
+    res.status(cache.status).set({
       ETag: cache.etag,
       "Content-Type": "application/json",
       "Last-Modified": cache.time.toUTCString(),
