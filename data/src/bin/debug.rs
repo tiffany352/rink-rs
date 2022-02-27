63fab65ff0eace01370ce6d53256690a3874c63a
Tiffany Bennett <tiffany@tiffnix.com>
2022-02-26 18:31:04 -0800

    Check memory usage of data

--- data/src/bin/debug.rs
+++ data/src/bin/debug.rs
@@ -1,5 +1,41 @@
-fn main() {
-    println!("size: {} bytes", rink_data::DATA.len());
+use std::{
+    alloc::{GlobalAlloc, System},
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
-    println!("{:#?}", rink_data::load_data());
+struct Alloc {
+    max_used: AtomicUsize,
+    system: System,
+}
+
+unsafe impl GlobalAlloc for Alloc {
+    unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        self.max_used.fetch_add(layout.size(), Ordering::Acquire);
+        self.system.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+        self.max_used.fetch_sub(layout.size(), Ordering::Release);
+        self.system.dealloc(ptr, layout)
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: Alloc = Alloc {
+    max_used: AtomicUsize::new(0),
+    system: System,
+};
+
+fn main() {
+    let start = ALLOCATOR.max_used.load(Ordering::SeqCst);
+    let data = rink_data::load_data();
+    let stop = ALLOCATOR.max_used.load(Ordering::SeqCst);
+
+    println!("{:?}", data);
+    println!("size: {} bytes", rink_data::DATA.len());
+    println!(
+        "memory: {} kB ({} kB more than start)",
+        stop / 1000,
+        (stop - start) / 1000
+    );
 }
