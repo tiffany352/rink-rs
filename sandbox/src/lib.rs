ab9c19b796bc8f9e4b1676130707cdea7465e1f8
Tiffany Bennett <tiffany@tiffnix.com>
2022-04-03 18:08:50 -0700

    License boilerplate all the files

--- sandbox/src/lib.rs
+++ sandbox/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! This crate provides a way to run code in a context with limited
 //! memory, execution time, and the ability to be interrupted.
 //!
