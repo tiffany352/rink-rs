e042aa052b91724629fb7e869046d8e5f562345d
Tiffany Bennett <tiffany@tiffnix.com>
2024-03-31 13:09:57 -0700

    only pull the parts of num that are actually used

--- core/src/types/bigint.rs
+++ core/src/types/bigint.rs
@@ -2,9 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use num::bigint::BigInt as NumInt;
-use num::cast::ToPrimitive;
-use num::traits::{Num, One, Zero};
+use num_bigint::BigInt as NumInt;
+use num_traits::{Num, One, ToPrimitive, Zero};
 use std::cmp::Ord;
 use std::fmt;
 use std::ops::{BitAnd, BitOr, BitXor, Div, Mul, Rem};
