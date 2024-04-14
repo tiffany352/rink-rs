a8a265cb130f8b4b94292aacb98dea55aec8fbb7
Tiffany Bennett <tiffany@tiffnix.com>
2024-04-13 23:14:42 -0700

    more improvements

--- web/repl/src/index.ts
+++ web/repl/src/index.ts
@@ -4,6 +4,19 @@ import init, * as rink from 'rink-js';
 const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
 const powRegex = /\^(\-?\d+)/g;
 
+type TokenType = "plain" | "error" | "unit" | "quantity" | "number" | "user_input" | "list_begin" | "list_sep" | "doc_string" | "pow" | "prop_name" | "date_time"
+
+type Token = {
+  type: "span",
+  text: string,
+  fmt: TokenType,
+};
+type TokenList = {
+  type: "list",
+  children: [SpanOrList],
+}
+type SpanOrList = Token | TokenList;
+
 function buildInline(parent: HTMLElement, text: string) {
   // escape any html tags
   parent.innerText = text;
@@ -18,7 +31,12 @@ function buildInline(parent: HTMLElement, text: string) {
   parent.innerHTML = text;
 }
 
-function buildHtml(tokens, parent: HTMLElement) {
+const dateFmt = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "long",
+  timeStyle: "long",
+})
+
+function buildHtml(tokens: [SpanOrList], parent: HTMLElement) {
   let ul: HTMLUListElement | null = null;
   let cur: HTMLElement = parent;
   for (const token of tokens) {
@@ -39,6 +57,12 @@ function buildHtml(tokens, parent: HTMLElement) {
       let li = document.createElement("li");
       cur = li;
       ul.appendChild(li);
+    } else if (token.fmt == "date_time") {
+      let time = document.createElement("time");
+      let date = new Date(token.text);
+      time.setAttribute("datetime", date.toISOString());
+      time.innerText = dateFmt.format(date);
+      cur.appendChild(time);
     } else {
       let span = document.createElement("span");
       span.classList.add(`hl-${token.fmt.replace('_', '-')}`);
@@ -61,6 +85,7 @@ init().then(() => {
 
   let welcome = document.createElement("p");
   welcome.innerText = `Rink ${rink.version()}`;
+  rinkDiv.innerHTML = '';
   rinkDiv.appendChild(welcome);
 
   form.addEventListener("submit", (event) => {
@@ -71,6 +96,7 @@ init().then(() => {
     rinkDiv.appendChild(quote);
 
     let query = new rink.Query(textEntry.value);
+    ctx.setTime(new Date());
     let tokens = ctx.eval_tokens(query);
     console.log(tokens);
 
@@ -79,5 +105,6 @@ init().then(() => {
 
     rinkDiv.appendChild(p);
     textEntry.value = "";
+    window.scrollTo(0, document.body.scrollHeight);
   });
 });
