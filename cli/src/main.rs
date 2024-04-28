8b70e552335b2a38c5a0169f53551b81e862002b
Tiffany Bennett <tiffany@tiffnix.com>
2024-04-27 20:00:58 -0700

    fix deprecations

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
 use eyre::{Result, WrapErr};
 use rink_sandbox::Alloc;
 use std::fs::File;
@@ -43,6 +43,7 @@ async fn main() -> Result<()> {
             Arg::new("config-path")
                 .long("config-path")
                 .help("Prints a path to the config file, then exits")
+                .action(ArgAction::SetTrue)
         )
         .arg(
             Arg::new("dump")
@@ -56,11 +57,12 @@ async fn main() -> Result<()> {
             Arg::new("service")
                 .long("service")
                 .help("Start in service mode")
+                .action(ArgAction::SetTrue)
                 .hide(true)
         )
         .get_matches();
 
-    if matches.is_present("service") {
+    if matches.get_flag("service") {
         return service::run_service();
     }
     // The panic handler can't be installed if entering service mode, so
@@ -68,7 +70,7 @@ async fn main() -> Result<()> {
     color_eyre::install()?;
     let config = config::read_config()?;
 
-    if let Some(filename) = matches.value_of("dump") {
+    if let Some(filename) = matches.get_one::<String>("dump") {
         use std::io::Write;
 
         let ctx = config::load(&config)?;
@@ -77,11 +79,11 @@ async fn main() -> Result<()> {
         return Ok(());
     }
 
-    if matches.is_present("config-path") {
+    if matches.get_flag("config-path") {
         println!("{}", config::config_path("config.toml").unwrap().display());
         Ok(())
-    } else if let Some(filename) = matches.value_of("file") {
-        match filename {
+    } else if let Some(filename) = matches.get_one::<&str>("file") {
+        match *filename {
             "-" => {
                 let stdin_handle = stdin();
                 repl::noninteractive(stdin_handle.lock(), &config, false)
@@ -91,7 +93,7 @@ async fn main() -> Result<()> {
                 repl::noninteractive(BufReader::new(file), &config, false)
             }
         }
-    } else if let Some(exprs) = matches.values_of("EXPR") {
+    } else if let Some(exprs) = matches.get_many::<String>("EXPR") {
         let mut ctx = config::load(&config)?;
         for expr in exprs {
             println!("> {}", expr);
