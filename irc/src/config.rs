9d5cb59bbccf9b1842f1945e4781282c6688f224
Tiffany Bennett <tiffany@tiffnix.com>
2024-05-27 19:04:54 -0700

    Fix the irc bot (#169)
    This has been broken for many years. I didn't have much motivation to
    fix it because I wasn't actively using irc anymore.
    
    Most of the libraries in the time in between have changed. I have to
    rewrite the bot from scratch.
    
    Unfortunately, the async ecosystem in rust is split down the middle with
    tokio vs async-std. `irc` uses tokio, `rink-sandbox` uses async-std, so
    they aren't compatible. I don't have the motivation to fix this yet, so
    for now the IRC bot has no sandboxing support.

---
+++ irc/src/config.rs
@@ -0,0 +1,106 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use irc::client::data::Config as IrcConfig;
+use rink_core::{ast, loader::gnu_units, parsing::datetime, Context};
+use serde_derive::{Deserialize, Serialize};
+use std::{path::PathBuf, time::Duration};
+use ubyte::ByteUnit;
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct Limits {
+    /// Whether support for sandboxing is enabled.
+    pub enabled: bool,
+    /// Whether performance metrics are shown after each query.
+    pub show_metrics: bool,
+    /// The maximum amount of heap that can be used per query, in megabytes.
+    pub memory: ByteUnit,
+    /// How long to wait before cancelling a query.
+    #[serde(with = "humantime_serde")]
+    pub timeout: Duration,
+}
+
+impl Default for Limits {
+    fn default() -> Self {
+        Limits {
+            enabled: true,
+            show_metrics: true,
+            memory: ByteUnit::Megabyte(20),
+            timeout: Duration::from_secs(10),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct Currency {
+    pub enabled: bool,
+    pub path: PathBuf,
+}
+
+impl Default for Currency {
+    fn default() -> Self {
+        Currency {
+            enabled: false,
+            path: PathBuf::from("./currency.json"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct Behavior {
+    pub follow_invites: bool,
+}
+
+impl Default for Behavior {
+    fn default() -> Self {
+        Behavior {
+            follow_invites: true,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+#[serde(default, deny_unknown_fields)]
+pub struct Config {
+    pub limits: Limits,
+    pub currency: Currency,
+    pub behavior: Behavior,
+    pub servers: Vec<IrcConfig>,
+}
+
+fn try_load_currency(config: &Currency, ctx: &mut Context) {
+    let base = rink_core::CURRENCY_FILE.unwrap();
+    let live = std::fs::read_to_string(&config.path).unwrap();
+
+    let mut base_defs = gnu_units::parse_str(&base);
+    let mut live_defs: ast::Defs = serde_json::from_str(&live).unwrap();
+
+    let mut defs = vec![];
+    defs.append(&mut base_defs.defs);
+    defs.append(&mut live_defs.defs);
+    ctx.load(ast::Defs { defs }).unwrap();
+}
+
+/// Creates a context by searching standard directories
+pub fn load(config: &Config) -> Context {
+    // Read definitions.units
+    let units = rink_core::DEFAULT_FILE.unwrap();
+    // Read datepatterns.txt
+    let dates = rink_core::DATES_FILE.unwrap();
+
+    let mut ctx = Context::new();
+    ctx.save_previous_result = true;
+    ctx.load(gnu_units::parse_str(&units)).unwrap();
+    ctx.load_dates(datetime::parse_datefile(&dates));
+
+    // Load currency data.
+    if config.currency.enabled {
+        try_load_currency(&config.currency, &mut ctx);
+    }
+
+    ctx
+}
--- irc/src/main.rs
+++ irc/src/main.rs
@@ -2,85 +2,98 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-extern crate glob;
-extern crate irc;
-extern crate rink;
+use futures::StreamExt;
+use irc::client::prelude::*;
+use rink_core::output::fmt::TokenFmt;
+use rink_sandbox::Alloc;
 
-fn main() {
-    use glob::glob;
-    use irc::client::prelude::*;
-    use rink::*;
-    use std::thread;
+mod config;
+mod fmt;
 
-    #[cfg(feature = "sandbox")]
-    fn eval(line: &str) -> String {
-        one_line_sandbox(line)
-    }
+#[global_allocator]
+pub(crate) static GLOBAL: Alloc = Alloc::new(usize::MAX);
 
-    #[cfg(not(feature = "sandbox"))]
-    fn eval(line: &str) -> String {
-        let mut ctx = load().unwrap();
-        ctx.short_output = true;
-        match one_line(&mut ctx, line) {
-            Ok(v) => v,
-            Err(e) => e,
-        }
-    }
+async fn server_task(config: config::Config, index: usize) {
+    let server = &config.servers[index];
+    let mut ctx = config::load(&config);
 
-    fn run(config: &str) {
-        let server = IrcServer::new(config).unwrap();
-        server.identify().unwrap();
-        let nick = server.config().nickname.clone().unwrap();
-        let mut prefix = nick.clone();
-        prefix.push(':');
-        for message in server.iter() {
-            if let Ok(Message {
-                command: Command::PRIVMSG(ref chan, ref message_str),
-                ..
-            }) = message
-            {
-                let prefixed = message_str.starts_with(&*prefix);
-                if prefixed || &*chan == &*nick {
-                    let reply_to = if &*chan == &*nick {
-                        message.as_ref().unwrap().source_nickname().unwrap()
-                    } else {
-                        &*chan
-                    };
-                    let line = if prefixed {
-                        message_str[prefix.len()..].trim()
-                    } else {
-                        &message_str[..]
-                    };
-                    let mut i = 0;
-                    let reply = eval(line);
-                    for line in reply.lines() {
-                        if !line.trim().is_empty() {
-                            server
-                                .send(Command::NOTICE(reply_to.to_owned(), line.to_owned()))
-                                .unwrap();
-                            i += 1;
-                        }
-                        // cut off early
-                        if i > 4 {
-                            break;
-                        }
-                    }
+    let servername = server.server.as_ref().map(|s| &s[..]).unwrap_or("");
+
+    let mut client = Client::from_config(server.clone()).await.unwrap();
+    println!("[{servername}] Connecting...");
+    client.identify().unwrap();
+    println!(
+        "[{servername}] Connected. nickname: {}",
+        client.current_nickname()
+    );
+
+    let mut stream = client.stream().unwrap();
+    while let Some(message) = stream.next().await.transpose().unwrap() {
+        let source = message.source_nickname();
+        if let Command::PRIVMSG(ref channel, ref text) = message.command {
+            let nick = client.current_nickname();
+            let prefix = format!("{}:", nick);
+            let command = if let Some(text) = text.strip_prefix(&prefix) {
+                text
+            } else if channel == client.current_nickname() {
+                &text
+            } else {
+                continue;
+            };
+            println!("[{servername}] <== {command}");
+            let result = rink_core::eval(&mut ctx, command);
+            let result = match &result {
+                Ok(res) => res.to_spans(),
+                Err(err) => err.to_spans(),
+            };
+            let result = fmt::to_irc_string(&config, &result);
+            println!("[{servername}] ==> {result}");
+            let where_to = if channel == client.current_nickname() {
+                if let Some(source) = source {
+                    source
+                } else {
+                    continue;
                 }
-            } else if let Err(e) = message {
-                println!("{}: {}", config, e);
+            } else {
+                &channel
+            };
+            client.send_notice(where_to, result).unwrap();
+        } else if let Command::INVITE(_nickname, channel) = message.command {
+            if config.behavior.follow_invites {
+                client.send_join(channel).unwrap();
             }
         }
-        println!("Thread {} exiting", config);
     }
+}
 
-    let mut threads = vec![];
-    for config in glob("servers/*.json").expect("Glob failed") {
-        match config {
-            Ok(config) => threads.push(thread::spawn(move || run(config.to_str().unwrap()))),
-            Err(e) => println!("{:?}", e),
+#[tokio::main]
+async fn main() {
+    let config_file = std::fs::read_to_string("config.toml").unwrap();
+    let config = toml::from_str::<config::Config>(&config_file).unwrap();
+
+    println!("Loading rink...");
+    let mut ctx = config::load(&config);
+
+    println!("Making sure we can actually run queries...");
+    let test_query = rink_core::eval(&mut ctx, "m");
+    match test_query {
+        Ok(_res) => (),
+        bad => {
+            println!("failed to run a basic query: {:#?}", bad);
+            std::process::exit(1);
         }
     }
-    for thread in threads {
-        thread.join().unwrap()
+
+    if config.servers.is_empty() {
+        println!("config.toml doesn't contain any servers");
+        std::process::exit(1);
+    }
+
+    let handles = (0..config.servers.len())
+        .map(|i| server_task(config.clone(), i))
+        .collect::<Vec<_>>();
+
+    for handle in handles {
+        handle.await;
     }
 }
