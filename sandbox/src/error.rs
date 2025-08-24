ae7ee7c23094f77cda8ff65b55370781110aa284
Tiffany Bennett <tiffany@tiffnix.com>
2025-08-23 23:36:38 -0700

    replace color-eyre in rink-sandbox + better errors

--- sandbox/src/error.rs
+++ sandbox/src/error.rs
@@ -12,12 +12,10 @@ use std::time::Duration;
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum Error {
+    /// Panic
+    Panic(PanicInfo),
     /// Timed out after {0:?}
     Timeout(Duration),
-    /// Panic: {0}
-    Panic(String),
-    /// When starting the child process fails
-    HandshakeFailure(String),
     /// Child process crashed
     Crashed,
     /// Interrupted (Ctrl+C)
@@ -25,6 +23,8 @@ pub enum Error {
     /// Read channel disconnected (other end hung up)
     Disconnected,
 
+    /// When starting the child process fails
+    HandshakeFailure(SerializableError),
     /// Failed to start child process
     InitFailure(IoError),
     /// Failed to read from child
@@ -42,15 +42,17 @@ pub enum Error {
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Error::Panic(panic_info) => write!(f, "{}", panic_info),
             Error::Timeout(duration) => write!(f, "Timed out after {duration:?}"),
-            Error::Panic(message) => write!(f, "Panic: {message}"),
-            Error::HandshakeFailure(message) => write!(f, "Failed to start subprocess: {message}"),
             Error::Crashed => write!(f, "Subprocess crashed"),
             Error::Interrupted => write!(f, "Interrupted"),
             Error::Disconnected => write!(f, "Disconnected"),
 
+            Error::HandshakeFailure(_) => {
+                write!(f, "Failed to start communicating with subprocess")
+            }
             Error::InitFailure(_) => write!(f, "Failed to start subprocess"),
             Error::ReadFailed(_) => write!(f, "Failed to read from subprocess"),
             Error::WriteFailed(_) => write!(f, "Failed to write to subprocess"),
@@ -76,14 +78,10 @@ impl From<Error> for IoError {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub(crate) enum ErrorResponse {
-    Panic(String),
-}
-
 impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
         match self {
+            Error::HandshakeFailure(error) => Some(error),
             Error::InitFailure(error) => Some(error),
             Error::ReadFailed(error) => Some(error),
             Error::WriteFailed(error) => Some(error),
@@ -97,5 +95,93 @@ impl StdError for Error {
 
     fn cause(&self) -> Option<&dyn StdError> {
         self.source()
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SerializableError {
+    display: String,
+    debug: String,
+    source: Option<Box<SerializableError>>,
+}
+
+impl fmt::Display for SerializableError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.display)
+    }
+}
+
+impl fmt::Debug for SerializableError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.debug)
+    }
+}
+
+impl StdError for SerializableError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        if let Some(ref source) = self.source {
+            Some(source)
+        } else {
+            None
+        }
+    }
+}
+
+impl SerializableError {
+    pub fn new<E>(error: E) -> Self
+    where
+        E: StdError,
+    {
+        SerializableError {
+            display: format!("{}", error),
+            debug: format!("{:?}", error),
+            source: if let Some(source) = error.source() {
+                Some(Box::new(SerializableError::new(source)))
+            } else {
+                None
+            },
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub(crate) enum ErrorResponse {
+    Panic(PanicInfo),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct PanicInfo {
+    pub(crate) message: Option<String>,
+    pub(crate) location: Option<LocationInfo>,
+    pub(crate) backtrace: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct LocationInfo {
+    pub(crate) file: String,
+    pub(crate) line: u32,
+}
+
+const BACKTRACE_OMITTED: &'static str = "
+
+Backtrace omitted.
+Run with RUST_BACKTRACE=1 environment variable to display it.
+Run with RUST_BACKTRACE=full to include source snippets.";
+
+impl fmt::Display for PanicInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The subprocess panicked (crashed).")?;
+        if let Some(ref message) = self.message {
+            write!(f, "\nMessage: {}", message)?;
+        }
+        if let Some(ref location) = self.location {
+            write!(f, "\nLocation: {}:{}", location.file, location.line)?;
+        }
+        if let Some(ref backtrace) = self.backtrace {
+            write!(f, "\n\nBacktrace:\n{backtrace}")?;
+        } else {
+            write!(f, "{BACKTRACE_OMITTED}")?;
+        }
+        Ok(())
     }
 }
